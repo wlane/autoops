696b9011dbb82bf0e58fe1f4a6e601695f17c6f0
xyf <564354487@qq.com>
2017-04-30 14:44:46 +0800

    add  dns-simple.py

---
+++ foundations/dns-simple.py
@@ -0,0 +1,44 @@
+# This Python file uses the following encoding: utf-8
+
+import dns.resolver
+import os
+import httplib
+
+iplist = []
+#appdomain = "www.tvjoy.cn"
+appdomain = "baidu.com"
+
+def get_iplist(domain=""):
+    try:
+        A = dns.resolver.query(domain, 'A')
+    except Exception,e:
+        print "dns resolver error:"+str(e)
+        return
+    for i in A.response.answer:
+        for j in i.items:
+            iplist.append(j.address)
+    return True
+
+def checkip(ip):
+    checkurl=ip+":80"
+    getcontent=""
+    httplib.socket.setdefaulttimeout(5)
+    conn=httplib.HTTPConnection(checkurl)
+    try:
+        conn.request("GET", "/", headers = {"Host": appdomain})
+        r=conn.getresponse()
+        #getcontent = r.read(15)
+        getcontent = r.read(6)
+    finally:
+        #if getcontent=="<!DOCTYPE html>":
+        if getcontent == "<html>":
+            print ip+" [ok]"
+        else:
+            print ip+" [ERROR]"
+
+if __name__=="__main__":
+    if get_iplist(appdomain) and len(iplist)>0:
+        for ip in iplist:
+            checkip(ip)
+    else:
+        print "dns resolver error"
