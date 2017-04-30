f88faba4879d55d5dbd39b96e33f12213e16d903
xyf <564354487@qq.com>
2017-04-30 13:41:14 +0800

    add dnsmodule

---
+++ foundations/dnsmodule.py
@@ -0,0 +1,23 @@
+# This Python file uses the following encoding: utf-8
+
+import dns.resolver
+
+domaina =  raw_input('please input an domain: ')    #使用一级域名，如baidu.com
+A = dns.resolver.query(domaina, 'A') #获取A记录
+print A.response
+print '\n'
+print A.response.answer[0]
+print '\n'
+print A.response.answer
+print '\n'
+for i in A.response.answer:
+    print i.items
+    for j in i.items:
+        print j.address
+
+domainb = raw_input('please input an domain: ') #使用二级域名，如www.baidu.com
+cname = dns.resolver.query(domainb, 'CNAME') #获取cname记录
+for i_c in cname.response.answer:
+    for j_c in i_c.items:
+        print j_c.to_text()
+
